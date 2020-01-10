6640507c743a497ca67729fc83d3e057883a0fa7
munirajrajammal <muniraj.rajammal@gmail.com>
2020-01-10 17:26:21 +0530

    complete of member db dump insert and password bcript and create txt-file

---
+++ memberPassword.js
@@ -0,0 +1,96 @@
+const mysql = require('mysql');
+var fs = require('fs');
+const bcrypt = require("bcrypt");
+
+// get db data
+const ourDb = mysql.createConnection({
+  host: 'localhost',
+  user: 'swftrustcheckExpenditure',
+  password: 'password',
+  database: 'swftrustNews'
+});
+ourDb.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('==== connect of our db ====');
+  getEmail().then((res) => {
+    insertData(res)
+  })
+});
+
+// insert member db data
+const myDb = mysql.createConnection({
+  host: 'localhost',
+  user: 'swfDumpCheckExpnditure',
+  password: 'password',
+  database: 'swfDumpCheck'
+});
+myDb.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('**** connect of mydb ******');
+});
+
+
+
+function getEmail() {
+  console.log('----- before check mysql ------')
+  return new Promise((resolve, reject) => {
+    getMember = 'select * from member';
+    ourDb.query(getMember, (err, result) => {
+      if (err) {
+        console.log('@@@ error of select db data @@@@', err)
+        reject(err)
+      } else {
+        // console.log('----- query of data member ------', result)
+        console.log('----- query of data member ------', result.length)
+        var stream = fs.createWriteStream("my_file.txt");
+        var memberData = result.map((data) => {
+          if (data['email'] === 'guest') {
+            console.log('guest data ', data['email'])
+            stream.write(` ======================== \n id : ${data['id']} , \n id_facebook : ${data['id_facebook']} , \n loginName : ${data['loginName']} , \n email : ${data['email']} , \n DOB : ${data['DOB']} , \n Per_mail : ${data['Per_mail']} , \n gender : ${data['gender']} , \n ${data['email']}\n`);
+          } else {
+            return {
+              id: data['id'],
+              email: data['email'],
+              userName: data['loginName'],
+              password: data['password']
+            }
+          }
+        })
+        resolve(memberData)
+        ourDb.end();
+      }
+    })
+  })
+}
+
+function insertData(res) {
+  console.log('kkkkkkkkdddd=================================', res)
+  res.map((insertData) => {
+    console.log('kkkkklllllll', insertData)
+    if (insertData === undefined) {
+      console.log('eeeerrrrr')
+    } else {
+      var saltRounds = 12;
+      console.log('$$$$$$$$ result of password $$$$$', insertData['password'])
+      bcrypt.hash(insertData['password'], saltRounds, function (err, hash) {
+        console.log('hashhhhhhhh', hash)
+        console.log('rrrrrrrrrrr', insertData['email'], insertData['userName'])
+        var role = 'member';
+        var source = 'admin';
+        var isActive = 1;
+        queryMember = 'INSERT INTO auth( userName , email , password , role , source , isActive ) VALUES ( ?,?,?,?,?,? )'
+        myDb.query(queryMember, [insertData['userName'], insertData['email'], hash, role, source, isActive], (err) => {
+          if (err) {
+            console.log('====== error ====')
+          } else {
+            console.log('====== insert successfully ======')
+          }
+        });
+      })
+    }
+  })
+}
